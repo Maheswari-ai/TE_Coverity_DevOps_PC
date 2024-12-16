5597f219b2ad6b53e23f9c267e12749c4962739c
EC2 Default User <ec2-user@ip-172-31-38-183.eu-north-1.compute.internal>
2024-12-16 23:54:03 +0000

    Build successful and updated application

---
+++ coverity/calculator-qml-dir/statemachine.h
@@ -0,0 +1,53 @@
+﻿//
+// Statemachine code from reading SCXML file 'statemachine.scxml'
+// Created by: The Qt SCXML Compiler version 2 (Qt 6.8.1)
+// WARNING! All changes made in this file will be lost!
+//
+
+#ifndef STATEMACHINE_H
+#define STATEMACHINE_H
+
+#include <QScxmlStateMachine>
+#include <QString>
+#include <QVariant>
+
+class CalculatorStateMachine: public QScxmlStateMachine
+{
+    /* qmake ignore Q_OBJECT */
+    Q_OBJECT
+    Q_PROPERTY(bool wrapper)
+    Q_PROPERTY(bool on)
+    Q_PROPERTY(bool ready)
+    Q_PROPERTY(bool begin)
+    Q_PROPERTY(bool result)
+    Q_PROPERTY(bool negated1)
+    Q_PROPERTY(bool operand1)
+    Q_PROPERTY(bool zero1)
+    Q_PROPERTY(bool int1)
+    Q_PROPERTY(bool frac1)
+    Q_PROPERTY(bool opEntered)
+    Q_PROPERTY(bool negated2)
+    Q_PROPERTY(bool operand2)
+    Q_PROPERTY(bool zero2)
+    Q_PROPERTY(bool int2)
+    Q_PROPERTY(bool frac2)
+
+
+public:
+    Q_INVOKABLE CalculatorStateMachine(QObject *parent = 0);
+    ~CalculatorStateMachine();
+
+
+
+Q_SIGNALS:
+
+
+private:
+    struct Data;
+    friend struct Data;
+    struct Data *data;
+};
+
+Q_DECLARE_METATYPE(::CalculatorStateMachine*)
+
+#endif // STATEMACHINE_H
